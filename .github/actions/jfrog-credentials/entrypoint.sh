3902d28c073873acbb9a2c253acfc8f1d61b66e8
Omkar Chavan <40963275+Omkarprakashchavan@users.noreply.github.com>
2024-06-13 15:38:34 +0530

    Update entrypoint.sh

--- .github/actions/jfrog-credentials/entrypoint.sh
+++ .github/actions/jfrog-credentials/entrypoint.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-if [[ "${JFROG_URL}" == "AURBA_URL" ]]; then
-  echo "Printing JFROG CRED for ARUBA"
-  echo "JFROG_USERNAME='ARUBA_USERNAME'" >> $GITHUB_OUTPUT
-  echo "JFROG_PASSWORD='ARUBA_PASSWORD')" >> $GITHUB_OUTPUT
-  # echo "JFROG_USERNAME=$(echo "${SECRETS}" | jq -r .ARUBA_JFROG_USERNAME)" >> $GITHUB_OUTPUT
-  # echo "JFROG_PASSWORD=$(echo "${SECRETS}" | jq -r .ARUBA_JFROG_PASSWORD)" >> $GITHUB_OUTPUT
-elif [[ "${JFROG_URL}" == "CSS_URL" ]]; then
-  echo "JFROG_USERNAME=$(echo "${SECRETS}" | jq -r .CSS_JFROG_USERNAME)" >> $GITHUB_OUTPUT
-  echo "JFROG_PASSWORD=$(echo "${SECRETS}" | jq -r .CSS_JFROG_PASSWORD)" >> $GITHUB_OUTPUT
-else
-  echo "JFROG_USERNAME=$(echo "${SECRETS}" | jq -r .ARUBA_JFROG_USERNAME)" >> $GITHUB_OUTPUT
-  echo "JFROG_PASSWORD=$(echo "${SECRETS}" | jq -r .ARUBA_JFROG_PASSWORD)" >> $GITHUB_OUTPUT
-fi
+# if [[ "${JFROG_URL}" == "AURBA_URL" ]]; then
+#   echo "Printing JFROG CRED for ARUBA"
+#   echo "JFROG_USERNAME='ARUBA_USERNAME'" >> $GITHUB_OUTPUT
+#   echo "JFROG_PASSWORD='ARUBA_PASSWORD')" >> $GITHUB_OUTPUT
+#   # echo "JFROG_USERNAME=$(echo "${SECRETS}" | jq -r .ARUBA_JFROG_USERNAME)" >> $GITHUB_OUTPUT
+#   # echo "JFROG_PASSWORD=$(echo "${SECRETS}" | jq -r .ARUBA_JFROG_PASSWORD)" >> $GITHUB_OUTPUT
+# elif [[ "${JFROG_URL}" == "CSS_URL" ]]; then
+#   echo "JFROG_USERNAME=$(echo "${SECRETS}" | jq -r .CSS_JFROG_USERNAME)" >> $GITHUB_OUTPUT
+#   echo "JFROG_PASSWORD=$(echo "${SECRETS}" | jq -r .CSS_JFROG_PASSWORD)" >> $GITHUB_OUTPUT
+# else
+#   echo "JFROG_USERNAME=$(echo "${SECRETS}" | jq -r .ARUBA_JFROG_USERNAME)" >> $GITHUB_OUTPUT
+#   echo "JFROG_PASSWORD=$(echo "${SECRETS}" | jq -r .ARUBA_JFROG_PASSWORD)" >> $GITHUB_OUTPUT
+# fi
